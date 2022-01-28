4af208b75e8b8df4a56f4b8f2a6ba62097f1c771
puneetsingh99 <99singhpuneet99@gmail.com>
2022-01-28 12:39:35 +0530

    add rds logo png

--- RDSApp/src/assets/images/Image.ts
+++ RDSApp/src/assets/images/Image.ts
@@ -2,6 +2,8 @@ const Images = {
   homeIcon: require('../../../assets/home.png'),
   taskIcon: require('../../../assets/task.png'),
   profileIcon: require('../../../assets/profile.png'),
+  //LOGO
+  rdsLogo: require('../../../assets/rdsLogo.png'),
 };
 
 export default Images;
