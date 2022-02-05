f4241fbd7e1f141739404b8dd8614cf0cc237999
palakgupta712 <palakgupta712@gmail.com>
2022-02-05 23:46:38 +0530

    task srceen added

--- src/screens/TaskScreen/styles.ts
+++ src/screens/TaskScreen/styles.ts
@@ -1,3 +1,9 @@
 import {StyleSheet} from 'react-native';
 
-export const TaskScreenView = StyleSheet.create({});
+export const TaskScreenView = StyleSheet.create({
+  text: {
+    padding: 20,
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
