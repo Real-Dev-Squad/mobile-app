143b2b1a2f3eec506fde40f79e284edc1ea187ab
dubey-shivani <shreyamishra040398@gmail.com>
2022-02-12 17:48:07 +0530

    added the default image

--- src/constants/images/Image.ts
+++ src/constants/images/Image.ts
@@ -12,7 +12,7 @@ const Images = {
   rdsLogo: require('../../../assets/rdsLogo.png'),
 
   //Default Image Url
-  PLACEHOLDER_IMAGE:
+  DEFAULT_IMAGE:
     'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
 };
 
