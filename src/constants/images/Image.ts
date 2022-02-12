ccaf0fd3e4ef7f4f8a042287c91518407686125e
dubey-shivani <shreyamishra040398@gmail.com>
2022-02-12 17:47:24 +0530

    added the default image

--- src/constants/images/Image.ts
+++ src/constants/images/Image.ts
@@ -10,6 +10,10 @@ const Images = {
 
   //LOGO
   rdsLogo: require('../../../assets/rdsLogo.png'),
+
+  //Default Image Url
+  PLACEHOLDER_IMAGE:
+    'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
 };
 
 export default Images;
