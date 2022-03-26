b6395a7bf3c45237173c2a834004309fe1fff952
dubey-shivani <shreyamishra040398@gmail.com>
2022-03-27 00:37:22 +0530

    fixed error

--- src/constants/appConstant/url.ts
+++ src/constants/appConstant/url.ts
@@ -1,4 +1,6 @@
-import {githubConfig} from '../../../config/config';
+// import {githubConfig} from '../../../config/config'; // you can include client id from readme file in config
+
+import {githubConfig} from '../../../config/config.sample';
 
 export const urls = {
   GITHUB_AUTH: `https://github.com/login/oauth/authorize?client_id=${githubConfig.clientId}`,
