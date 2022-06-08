cde69cbffc78ce4b127fb1a4cf1551c68329b100
shreya-mishra <shreyamishra040398@gmail.com>
2022-06-08 20:29:17 +0530

    added the test folder

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { AuthProvider } from './src/context/AuthContext';
 import Index from './src';
+import { Text } from 'react-native';
 
 const App = () => {
   return (
     <AuthProvider>
       <Index />
     </AuthProvider>
+ 
   );
 };
 
