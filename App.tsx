6fce6e543fa31c779d052a83ea844dca752f5d5a
Saksham Bhatt <sakshambhatt33@gmail.com>
2022-06-11 21:24:25 +0530

    renamed index->Index, added App-test

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { AuthProvider } from './src/context/AuthContext';
-import Index from './src';
+import Index from './src/Index';
 
 const App = () => {
   return (
