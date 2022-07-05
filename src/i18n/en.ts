901b63e0df0bb8b043da38a3bc5fb7548e59a559
Maheima <mahi.khandelwal28@gmail.com>
2022-07-05 21:16:20 +0530

    Pinned yarn and node using Volta and updated package.json

--- src/i18n/en.ts
+++ src/i18n/en.ts
@@ -11,7 +11,7 @@ const Strings = {
   ActiveBtn_Text: 'change status to Active',
   OOOBtn1_Text: 'Mark Yourself as OOO',
   OOOBtn2_Text: 'Mark Yourself as Active again',
-  
+
   // Profile Screen
   Greet_User: 'Hello User',
   Img_Upload_Text: 'Upload your Profile Image',
