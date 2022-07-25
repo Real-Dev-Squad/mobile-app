5b340b4815e7432eb682eef1a4fbcbdb6847a107
cyrus25 <cyruspassi@gmail.com>
2022-07-26 00:00:50 +0530

    renamed Index.tsx file

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { AuthProvider } from './src/context/AuthContext';
 import Toast from 'react-native-toast-message';
-import Index from './src/Index';
+import Index from './src';
 
 const App = () => {
   return (
