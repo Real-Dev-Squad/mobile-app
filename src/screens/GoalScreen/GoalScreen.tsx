d76285210c454c482115f4abceae3a7020a60f11
priyanshk20 <khandelwalpriyansh85@gmail.com>
2022-07-26 13:54:49 +0530

    reverted the changes made while see the component on screen

--- src/screens/GoalScreen/GoalScreen.tsx
+++ src/screens/GoalScreen/GoalScreen.tsx
@@ -1,21 +1,14 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View} from 'react-native';
 import withHeader from '../../helpers/withHeader';
 import ShortGoalsComponent from '../../components/ShortGoalsComponent/ShortGoalsComponent';
-import CreateGoalsComponent from '../../components/CreateGoalForm/CreateGoalform'
 const GoalScreen = () => {
   return (
     <View style={styles.contain}>
-      <CreateGoalsComponent />
+      <ShortGoalsComponent />
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  contain:{
-    height:'100%',
-  },
-})
 
 export default withHeader(GoalScreen);
 
