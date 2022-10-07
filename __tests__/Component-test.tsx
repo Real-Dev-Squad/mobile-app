4fff2a05fe8125f4e3bbf4e00b275c2f74be122e
vinayak <trivedivinayak57@gmail.com>
2022-10-07 10:40:34 +0530

    added solution for failing tests

--- __tests__/Component-test.tsx
+++ __tests__/Component-test.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import {fireEvent, render} from "@testing-library/react-native"
-import ShortGoalsComponent from "../src/components/ShortGoalsComponent/ShortGoalsComponent";
-import Card from "../src/components/ToDoComponent/Card"
-import Data from "../src/components/ToDoComponent/Data"
-import 'react-native-gesture-handler'
+import React from 'react';
+import { fireEvent, render } from '@testing-library/react-native';
+import ShortGoalsComponent from '../src/components/ShortGoalsComponent/ShortGoalsComponent';
+import Card from '../src/components/ToDoComponent/Card';
+import Data from '../src/components/ToDoComponent/Data';
+import 'react-native-gesture-handler';
 // Short Term Goals component test
 
 test('flatlist does not exist on initial render', () => {
@@ -30,41 +30,46 @@ test('flatlist exists when we click on the arrow button', () => {
 
 // ToDoComponent Tests
 
-jest.mock('react-native-gesture-handler', () => {})
+jest.mock('react-native-gesture-handler', () => {});
 
 jest.mock('react-native-gesture-handler', () => {
-    const View = require('react-native').View;
-    
-    return {
-        GestureHandlerRootView: View,
-        PanGestureHandler: View,
-    }
+  const View = require('react-native').View;
+
+  return {
+    GestureHandlerRootView: View,
+    PanGestureHandler: View,
+  };
 });
-jest.mock("react-native-reanimated", () => require("react-native-reanimated/mock") );
+jest.mock('react-native-reanimated', () =>
+  require('react-native-reanimated/mock'),
+);
 jest.mock('react-native-reanimated', () => {
-    const View = require('react-native').View;
-  
-    return {
-      useSharedValue: jest.fn(),
-      useAnimatedGestureHandler: jest.fn(),
-      useAnimatedStyle: jest.fn(),
-      View: View,
-    };
-  });
+  const View = require('react-native').View;
+
+  return {
+    useSharedValue: jest.fn(),
+    useAnimatedGestureHandler: jest.fn(),
+    useAnimatedStyle: jest.fn(),
+    View: View,
+    Easing: {},
+  };
+});
 
 test('setTimeout called which calls other two functions remove and changecard', () => {
-    const {getByTestId,} = render(
-      <Card 
-        item={Data[0]}
-        posStyle='relative'
-        changecard={() => {}}
-        removeCard={() => {}}
-      />
-    )
+  const { getByTestId } = render(
+    <Card
+      item={Data[0]}
+      posStyle="relative"
+      changecard={() => {}}
+      removeCard={() => {}}
+      disabled={false}
+      setDisabled={() => {}}
+    />,
+  );
 
-    jest.useFakeTimers()
-    jest.spyOn(global, 'setTimeout');
-    fireEvent.press(getByTestId('doneBtn'))
-    expect(setTimeout).toHaveBeenCalledTimes(1);
-    expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 4000);
-})
+  jest.useFakeTimers();
+  jest.spyOn(global, 'setTimeout');
+  fireEvent.press(getByTestId('doneBtn'));
+  expect(setTimeout).toHaveBeenCalledTimes(1);
+  expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 4000);
+});
