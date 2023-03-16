cc6c1f7722f981d2e101c3605f84df59583de398
Saksham Bhatt <sakshambhatt33@gmail.com>
2023-03-16 21:36:26 +0530

    fix: made the app run by chaning dep versions, manifest, and environment setup

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
+    [
+      'module:react-native-dotenv',
+      {
+        safe: true,
+      },
+    ],
     // react-native-reanimated plugin should be the last plugin
     [
       'react-native-reanimated/plugin',
