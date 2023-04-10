a8a3c9bdd23742725b384e2178dea5c5c7759f15
Saksham Bhatt <sakshambhatt33@gmail.com>
2023-04-10 21:37:53 +0530

    refactor: removed GithuView & added logic for toast in handleSignIn

--- src/screens/AuthScreen/AuthScreen.tsx
+++ src/screens/AuthScreen/AuthScreen.tsx
@@ -1,32 +1,19 @@
-import React, { useContext, useState } from 'react';
-import {
-  Text,
-  View,
-  TouchableOpacity,
-  Image,
-  ScrollView,
-  ActivityIndicator,
-  SafeAreaView,
-} from 'react-native';
-import WebView from 'react-native-webview';
-import { urls } from '../../constants/appConstant/url';
-import { AuthContext } from '../../context/AuthContext';
-import Images from '../../constants/images/Image';
-import { storeData } from '../../utils/dataStore';
+import React, { useState } from 'react';
+import { Text, View, TouchableOpacity, Image, ScrollView } from 'react-native';
 import Strings from '../../i18n/en';
 import { AuthViewStyle } from './styles';
 import { AuthScreenButton } from './Button';
 import { OtpModal } from './OtpModal';
-import { getUserData } from './Util';
+import { Toast } from 'react-native-toast-message/lib/src/Toast';
 
 const AuthScreen = () => {
-  const { setLoggedInUserData } = useContext(AuthContext);
-  const [githubView, setGithubView] = useState<boolean>(false);
+  // const { setLoggedInUserData } = useContext(AuthContext);
+  // const [githubView, setGithubView] = useState<boolean>(false);
   const [otpCode, setOtpCode] = useState<string>('');
   const [otpModalVisible, setOtpModalVisible] = useState<boolean>(false);
-  const [addressbarURL, setAdressbarURL] = useState<String>('');
-  const [loading, setLoading] = useState(false);
-  const [key, setKey] = useState(1);
+  // const [addressbarURL, setAdressbarURL] = useState<String>('');
+  // const [loading, setLoading] = useState(false);
+  // const [key, setKey] = useState(1);
 
   const closeModal = () => {
     setOtpModalVisible(false);
@@ -37,77 +24,15 @@ const AuthScreen = () => {
   //TODO: add to constants
   const maxLength = 4;
   const handleSignIn = () => {
-    setGithubView(true);
+    // NOTE: toast until sign in with Github is implemented
+    Toast.show({
+      type: 'info',
+      text1: 'Sign in with GitHub coming soon...',
+      position: 'bottom',
+      bottomOffset: 80,
+    });
   };
 
-  if (githubView) {
-    return (
-      <SafeAreaView style={{ flex: 1 }}>
-        <ScrollView contentContainerStyle={AuthViewStyle.container}>
-          <View style={AuthViewStyle.addressBarStyle}>
-            {loading ? (
-              <ActivityIndicator
-                style={{ marginLeft: 5 }}
-                size={25}
-                color="#fff"
-              />
-            ) : (
-              <TouchableOpacity onPress={() => setGithubView(false)}>
-                <Text style={AuthViewStyle.addressBarCancel}>Cancel</Text>
-              </TouchableOpacity>
-            )}
-            <Text style={AuthViewStyle.addressBarLink}>{addressbarURL}</Text>
-            {loading ? null : (
-              <TouchableOpacity onPress={() => setKey(key + 1)}>
-                <Image
-                  source={Images.refreshIcon}
-                  style={AuthViewStyle.addressBarIcon}
-                />
-              </TouchableOpacity>
-            )}
-          </View>
-          <WebView
-            key={key}
-            onNavigationStateChange={({ url }) => {
-              (async function () {
-                if (url === urls.REDIRECT_URL) {
-                  setAdressbarURL(url);
-                  try {
-                    const res = await getUserData(url);
-                    await storeData('userData', JSON.stringify(res));
-
-                    setLoggedInUserData({
-                      id: res?.id,
-                      name: res?.name,
-                      profileUrl: res?.profileUrl,
-                      status: res?.status,
-                    });
-                  } catch (err) {
-                    setLoggedInUserData(null);
-                  }
-                } else if (url.indexOf('?') > 0) {
-                  let uri = url.substring(0, url.indexOf('?'));
-                  setAdressbarURL(uri);
-                } else {
-                  setAdressbarURL(url);
-                }
-              })();
-            }}
-            style={AuthViewStyle.webViewStyles}
-            source={{
-              uri: urls.GITHUB_AUTH,
-            }}
-            onLoadStart={() => {
-              setLoading(true);
-            }}
-            onLoadEnd={() => {
-              setLoading(false);
-            }}
-          />
-        </ScrollView>
-      </SafeAreaView>
-    );
-  }
   //TODO: fix layout change on otp input
   return (
     <ScrollView contentContainerStyle={AuthViewStyle.container}>
