4ed34704de0b7cbaa67f3b666821ffda90e4be6a
Saksham Bhatt <sakshambhatt33@gmail.com>
2023-05-08 22:08:43 +0530

    refactor: removed tests for OTP Modal, added test for AuthScreen coming soon toast

--- __tests__/Component-test.tsx
+++ __tests__/Component-test.tsx
@@ -10,7 +10,6 @@ import Strings from '../src/i18n/en';
 import AuthScreen from '../src/screens/AuthScreen/AuthScreen';
 import { OtpModal } from '../src/screens/AuthScreen/OtpModal';
 
-
 // Short Term Goals component test
 
 test('flatlist does not exist on initial render', () => {
@@ -35,29 +34,29 @@ test('flatlist exists when we click on the arrow button', () => {
   });
 });
 
-
 // Create Goal component test
 
-
 test('roadmap type does not exist on initial render', () => {
-    const {queryByTestId} = render(<DurationDropdown/>);
-    expect(queryByTestId('longTermRoadmap')).toBeNull();
-})
+  const { queryByTestId } = render(<DurationDropdown />);
+  expect(queryByTestId('longTermRoadmap')).toBeNull();
+});
 
- test('roadmap type exists when use select duration as long term and stays undefined initally', ()=>{
-    const {getByTestId} = render(<DurationDropdown/>);
-    fireEvent.press(getByTestId('roadmapType'));
-    expect(getByTestId('roadmapType').props.value).toStrictEqual(undefined);
- })
+test('roadmap type exists when use select duration as long term and stays undefined initally', () => {
+  const { getByTestId } = render(<DurationDropdown />);
+  fireEvent.press(getByTestId('roadmapType'));
+  expect(getByTestId('roadmapType').props.value).toStrictEqual(undefined);
+});
 
- // Floating Button test
+// Floating Button test
 
- test('floating actions exists when we click floating button', ()=> {
-    const {queryByTestId, getByTestId} = render(<FloatingButton/>);
-    fireEvent.press(getByTestId('floatingButton'))
-    expect(queryByTestId('floatingButton')).toBeTruthy();
-    expect(getByTestId('floatingButton').props.style).toStrictEqual({marginTop: '120%'})
- })
+test('floating actions exists when we click floating button', () => {
+  const { queryByTestId, getByTestId } = render(<FloatingButton />);
+  fireEvent.press(getByTestId('floatingButton'));
+  expect(queryByTestId('floatingButton')).toBeTruthy();
+  expect(getByTestId('floatingButton').props.style).toStrictEqual({
+    marginTop: '120%',
+  });
+});
 
 // ToDoComponent Tests
 
@@ -103,39 +102,4 @@ test('setTimeout called which calls other two functions remove and changecard', 
   fireEvent.press(getByTestId('doneBtn'));
   expect(setTimeout).toHaveBeenCalledTimes(1);
   expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 4000);
-});
-
-//Sign In Withh Web and OtpModal Component Test
-test('Check is sign in with web and otpmodal is rendering', () => {
-  const { getByText, queryByTestId } = render(<AuthScreen />);
-  const SignInWithWeb = getByText(Strings.SIGN_IN_WITH_WEB);
-  expect(SignInWithWeb).toBeTruthy();
-  expect(queryByTestId('otpModal')).toBeNull();
-  fireEvent.press(SignInWithWeb);
-  expect(queryByTestId('otpModal')).toBeTruthy();
-});
-
-test('Check otpmodal submit to be disabled and enabled', () => {
-  const props = {
-    title: Strings.ENTER_4_DIGIT_OTP,
-    testId: 'otpModal',
-    visible: true,
-    code: '',
-    setCode: () => {},
-    closeModal: () => {},
-  };
-  const data = [
-    { code: '', disabled: true },
-    { code: '12', disabled: true },
-    { code: '1234', disabled: false },
-  ];
-  data.forEach(({ code, disabled }) => {
-    const { getByTestId } = render(
-      <OtpModal maxLength={4} {...props} code={code} />,
-    );
-    expect(getByTestId('submitOtpModal').props).toHaveProperty(
-      'accessibilityState',
-      { disabled },
-    );
-  });
 });
