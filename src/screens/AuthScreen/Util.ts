9b5c3c4fb1ad2eee2f58b22bced1cb1a0edb3034
mohammadkhizerkhan <mohammad.khizer@m2pfintech.com>
2023-07-08 13:49:05 +0530

    refactor - removing fetch user data funcion from auth utils

--- src/screens/AuthScreen/Util.ts
+++ src/screens/AuthScreen/Util.ts
@@ -20,29 +20,6 @@ export const getUserData = async (url: string) => {
   }
 };
 
-export const fetchUserData = async (userId: string): Promise<User | null> => {
-  try {
-    const response = await axios.get(urls.GET_USER_DATA + userId, {
-      headers: {
-        cookie: '',
-      },
-    });
-    const user = response.data.user;
-    return {
-      name: user.github_display_name,
-      profileUrl: user.picture?.url,
-      userName: user.username,
-      designation: user.designation,
-      company: user.company,
-      linkedInUrl: urls.LINKEDIN + user.linkedin_id,
-      twitterUrl: urls.GITHUB + user.twitter_id,
-      githubUrl: urls.TWITTER + user.github_id,
-    };
-  } catch (error) {
-    return null;
-  }
-};
-
 export const fetchContribution = async (userName: string): Promise<any> => {
   try {
     const response = await axios.get(urls.GET_CONTRIBUTIONS + userName, {
