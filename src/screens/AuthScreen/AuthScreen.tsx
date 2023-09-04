ad9ed65170a41d2b53dbe225e6e8b10454682d52
mohammadkhizerkhan <mohammad.khizer@m2pfintech.com>
2023-09-04 23:26:55 +0530

    added encrypted storage methods

--- src/screens/AuthScreen/AuthScreen.tsx
+++ src/screens/AuthScreen/AuthScreen.tsx
@@ -15,7 +15,7 @@ import { AuthScreenButton } from './Button';
 import { Toast } from 'react-native-toast-message/lib/src/Toast';
 import { AuthContext } from '../../context/AuthContext';
 import { getUserData } from './Util';
-import { storeData } from '../../utils/dataStore';
+import { Storage, storeData } from '../../utils/dataStore';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { ActivityIndicator } from 'react-native';
 import Images from '../../constants/images/Image';
@@ -258,7 +258,6 @@ const AuthScreen = () => {
           onPress={activateCamera}
         />
       </View>
-
       {cameraActive && (
         <CameraScreen
           style={StyleSheet.absoluteFill}
