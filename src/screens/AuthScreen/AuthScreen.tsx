1922076631cf1152638db4a8d330f5957f64faed
Shivangi <shivangit1111@gmail.com>
2023-10-09 19:15:58 +0530

    feat: redirect user to AuthScreen

--- src/screens/AuthScreen/AuthScreen.tsx
+++ src/screens/AuthScreen/AuthScreen.tsx
@@ -5,6 +5,7 @@ import {
   View,
   TouchableOpacity,
   Image,
+  BackHandler,
   ScrollView,
   StyleSheet,
   Alert,
@@ -66,6 +67,17 @@ const AuthScreen = () => {
     try {
       //await requestCameraPermission();
       setCameraActive((prev) => !prev); // Set cameraActive state to true
+
+      const backAction = () => {
+        console.log('backClicked');
+        setCameraActive(false);
+        return true;
+      };
+      const backHandler = BackHandler.addEventListener(
+        'hardwareBackPress',
+        backAction,
+      );
+      return () => backHandler.remove();
     } catch (error: any) {
       Alert.alert('Error requesting camera permission:', error);
     }
