d9f21a892a9cf0491446084942ac67e047d4a558
Shivangi <shivangit1111@gmail.com>
2023-10-09 19:30:22 +0530

    feat:add alert box before exit the app

--- src/screens/AuthScreen/AuthScreen.tsx
+++ src/screens/AuthScreen/AuthScreen.tsx
@@ -71,6 +71,14 @@ const AuthScreen = () => {
       const backAction = () => {
         console.log('backClicked');
         setCameraActive(false);
+        Alert.alert('Hold on!', 'Are you sure you want to go back?', [
+          {
+            text: 'Cancel',
+            onPress: () => null,
+            style: 'cancel',
+          },
+          { text: 'YES', onPress: () => BackHandler.exitApp() },
+        ]);
         return true;
       };
       const backHandler = BackHandler.addEventListener(
