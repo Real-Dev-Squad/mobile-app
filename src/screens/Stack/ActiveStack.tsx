627a6cd9640c83762e513e3440a78ab883b816ea
Shivangi <shivangit1111@gmail.com>
2023-11-20 09:55:11 +0530

    removed unused ActivitScreen

--- src/screens/Stack/ActiveStack.tsx
+++ src/screens/Stack/ActiveStack.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import ProfileScreen2 from '../ProfileScreen/ProfileScreen2';
 import ActiveTaskDetail from '../ProfileScreen/DetailScreen/ActiveTaskDetail';
-import ActiveTaskDetail2 from '../ProfileScreen/DetailScreen/ActiveTaskDetail2';
 import ExtensionRequest from '../ProfileScreen/DetailScreen/ExtensionRequest';
 
 const Stack = createStackNavigator();
@@ -15,7 +14,6 @@ function ActiveTaskScreenStack() {
     >
       <Stack.Screen name="Profile" component={ProfileScreen2} />
       <Stack.Screen name="ActiveTaskDetail" component={ActiveTaskDetail} />
-      {/* <Stack.Screen name="ActiveTaskDetail2" component={ActiveTaskDetail2} /> */}
       <Stack.Screen name="ExtensionRequest" component={ExtensionRequest} />
     </Stack.Navigator>
   );
