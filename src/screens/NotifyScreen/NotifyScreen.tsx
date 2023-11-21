91ad90401f0c6379a94ae05b63f7c976d478cb2d
shreya-mishra <shreyamishra040398@gmail.com>
2023-11-21 14:19:42 +0530

    adding notify screen

---
+++ src/screens/NotifyScreen/NotifyScreen.tsx
@@ -0,0 +1,27 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import NotifyButton from '../../components/Notify/NotifyButton';
+import Colors from '../../constants/colors/Colors';
+
+const NotifyScreen = () => {
+  const onNotifyHandler = () => {
+    console.log('notify');
+  };
+  return (
+    <View style={styles.container}>
+      <NotifyButton
+        onPress={onNotifyHandler}
+        title={'Notify'}
+        buttonStyle={{ backgroundColor: Colors.Primary_Color }}
+        textStyle={{ color: 'white' }}
+      />
+    </View>
+  );
+};
+
+export default NotifyScreen;
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+});
