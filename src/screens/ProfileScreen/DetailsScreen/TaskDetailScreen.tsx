8f12345a132b918b889fa82e0dfd9aa6d2782438
shreya-mishra <shreyamishra040398@gmail.com>
2024-01-17 06:04:40 +0530

    added task detail screen

---
+++ src/screens/ProfileScreen/DetailsScreen/TaskDetailScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const TaskDetailScreen = () => {
+  return (
+    <View>
+      <Text>TaskDetailScreen</Text>
+    </View>
+  );
+};
+
+export default TaskDetailScreen;
+
+const styles = StyleSheet.create({});
