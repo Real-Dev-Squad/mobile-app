2d95298cb307cfd062b86ab2d393b46e420048eb
shreya-mishra <shreyamishra040398@gmail.com>
2024-03-20 00:43:36 +0530

    added all the components in invite screen

--- src/screens/Calendar/CalendarInviteScreen.tsx
+++ src/screens/Calendar/CalendarInviteScreen.tsx
@@ -1,13 +1,17 @@
-import { Alert, StyleSheet, View } from 'react-native';
+import { Alert, ScrollView, StyleSheet, View } from 'react-native';
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../context/AuthContext';
 import { fetchUsers } from '../../utils/Api';
 import { UserInfoType } from '../../context/type';
 import DropDown from '../../components/DropDown';
+import TimeZone from '../../components/CalendarSpecificComp/TimeZone';
+import DisplayProfile from '../../components/CalendarSpecificComp/DisplayProfile';
+import { windowHeight } from '../../helpers/CalendarInviteHelpers';
 
 const CalendarInviteScreen = () => {
   const { loggedInUserData } = useContext(AuthContext);
   const [users, setUsers] = useState<UserInfoType[]>([]);
+  const [selectedUser, setSelectedUser] = useState<UserInfoType[]>([]);
 
   useEffect(() => {
     loggedInUserData && fetchUsers(loggedInUserData?.token, setUsers);
@@ -17,31 +21,59 @@ const CalendarInviteScreen = () => {
   const handleUserIdChange = (info: UserInfoType) => {
     const userExists = users.some((user: UserInfoType) => user.id === info.id);
     if (!userExists) {
-      setUsers((prevUsers: any[]) => [...prevUsers, info]);
+      setSelectedUser((prevUsers: UserInfoType[]) => [...prevUsers, info]);
     } else {
       Alert.alert('user already exist');
     }
   };
   return (
-    <View style={styles.flexView}>
-      <View style={styles.dropdown}>
-        <DropDown
-          title={'Select To invite'}
-          handleUserId={handleUserIdChange}
-          error={''}
-          disabled={false}
-        />
-      </View>
-    </View>
+    <>
+      {/* Floating button */}
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={styles.scrollContainer}
+        stickyHeaderIndices={[0]}
+      >
+        <View style={styles.topHeader}>
+          {/* zoom-scale */}
+          <View style={styles.flexView}>
+            <View style={styles.dropdown}>
+              <DropDown
+                title={'Select To invite'}
+                handleUserId={handleUserIdChange}
+                error={''}
+                disabled={false}
+              />
+            </View>
+            {/* checkbox */}
+          </View>
+
+          <TimeZone />
+          <DisplayProfile
+            setSelectedUsers={setSelectedUser}
+            selectedUsers={selectedUser}
+            multiModeOn={false}
+          />
+        </View>
+        <View style={{ height: '100%' }} />
+      </ScrollView>
+    </>
   );
 };
 
 export default CalendarInviteScreen;
 const styles = StyleSheet.create({
+  scrollContainer: {
+    flex: 1,
+    overflow: 'scroll',
+    backgroundColor: 'white',
+    height: windowHeight,
+  },
   flexView: {
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between',
   },
   dropdown: { width: '70%' },
+  topHeader: { position: 'relative', top: 0, backgroundColor: 'white' },
 });
