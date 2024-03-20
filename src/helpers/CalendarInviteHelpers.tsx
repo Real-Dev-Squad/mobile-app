670a9e286f0bf56443720b41a4607333cbfe0394
shreya-mishra <shreyamishra040398@gmail.com>
2024-03-20 15:05:08 +0530

    calendarUI/table_timeSlotView

--- src/helpers/CalendarInviteHelpers.tsx
+++ src/helpers/CalendarInviteHelpers.tsx
@@ -3,6 +3,7 @@ import { format } from 'date-fns';
 
 export const windowWidth = Dimensions.get('window').width;
 export const windowHeight = Dimensions.get('window').height;
-export const todaysDate = format(new Date(), 'yyyy-MM-dd');
+export const CELL_HEIGHT = 60;
+export const todaysDate = format(new Date(), 'dd-MM-yyyy');
 export const formatDate = (date: string) =>
-  format(new Date(date), 'yyyy-MM-dd');
+  format(new Date(date), 'dd-MM-yyyy');
