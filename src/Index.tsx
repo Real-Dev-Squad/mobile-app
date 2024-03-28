92d86f5572108a2de75471caf9d568c599e5c052
Ashif Khan <ashifkhn02@gmail.com>
2024-03-28 14:03:00 +0530

    offline app package issue

--- src/Index.tsx
+++ src/Index.tsx
@@ -3,9 +3,12 @@ import { AuthContext } from './context/AuthContext';
 import LoadingScreen from './components/LoadingScreen';
 import TabNavigation from './navigations/TabNavigation/TabNavigation';
 import AuthScreen from './screens/AuthScreen/AuthScreen';
+import { useIsConnected } from 'react-native-offline';
 
 const Index = () => {
   const { isLoading, loggedInUserData } = useContext(AuthContext);
+  console.log(useIsConnected)
+
   if (isLoading) {
     return <LoadingScreen />;
   }
---
+++ src/screens/ConnectionScreen/ConnectionScreen.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { NetworkConsumer } from 'react-native-offline';
+
+const ConnectionScreen = () => {
+  return (
+    <View>
+      <Text>ConnectionScreen</Text>
+    </View>
+  )
+}
+
+export default ConnectionScreen
